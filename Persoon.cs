b4164204ef61add3a324cba1d41c015a5f9eba30
Martijn Wieggers <127099600+martijnwieggers@users.noreply.github.com>
2024-01-23 13:03:11 +0100

    Add project files.

---
+++ Persoon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blok4Les1
+{
+    public class Persoon
+    {
+        //Stap 1: Maak hier de publieke eigenschappen aan:
+        //AchterNaam (Tekst),
+        //Voornaam (Tekst)
+        //Leeftijd (Geheel getal).
+       
+        //Stap 2: Maak hier de methode VraagInfo()
+       
+        //Stap 3: Maak hier de methode ToonInfo();
+        
+
+
+
+
+
+        //Default Constructor, laten staan
+        public Persoon()
+        {
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Blok4Les1;
+
+List<Persoon> personen = new List<Persoon>();
+
+
+//Stap 4: Maak hier binnen een for-loop 3 personen aan en voeg deze toe aan de lijst "personen"
+
+//maak een foreach loop on voor alle personen in de lijst ToonInfo() aan te roepen
+
+
